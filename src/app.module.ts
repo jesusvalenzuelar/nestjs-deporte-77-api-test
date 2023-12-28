f346da5ce35004e4b73b463c3f3111dc67ac0a93
Jesus Gilberto Valenzuela Rochin <jesusvalenzuelar@gmail.com>
2023-12-28 09:00:56 -0700

    Adding env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
 import { Article } from './article/entities/article.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [
+        ConfigModule.forRoot({cache: true}),
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'JVServer2022',
+            host: process.env.DATABASE_HOST || 'localhost',
+            port: parseInt(process.env.DATABASE_PORT) || 5432,
+            username: process.env.DATABASE_USERNAME,
+            password: process.env.DATABASE_PASSWORD,
             entities: [Article],
-            database: 'Deporte77Blog',
+            database: process.env.DATABASE_NAME,
             synchronize: true,
             logging: true
         }),
